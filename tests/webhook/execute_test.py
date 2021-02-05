189dd00804962b8acb5153d4f841cc1463a870d4
thiagola92 <thiagola92@gmail.com>
2021-02-05 01:29:15 -0300

    Update test
    Not much, just moved the string from a variable to parameter

--- tests/webhook/execute_test.py
+++ tests/webhook/execute_test.py
@@ -2,8 +2,6 @@ import os
 import time
 from a_discord_webhook import Webhook
 
-text = 'message 1'
-webhook = Webhook(os.environ['URL']).set_content(text).execute()
-
+webhook = Webhook(os.environ['URL']).set_content('message 1').execute()
 time.sleep(5)
 webhook.set_content('message 2').execute()
